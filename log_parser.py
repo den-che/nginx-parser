f0acc304a12243b0a8cca3ed627b41432ffbe706
Denis Cherednikov <denis.cherednikov@gmail.com>
2017-10-10 10:53:34 +0300

    Delete almost all iterations:create new function main_parser_nginx

--- log_parser.py
+++ log_parser.py
@@ -5,24 +5,28 @@ import os
 import sys
 
 
-def load_log_file(file_path):
-    nginx_log_list = []   
+def main_parser_nginx(file_path):
+    code_500 = []
+    count_method_code = 0   
     with open(file_path, 'r') as log_file:
         for line in log_file:
-        #Can't avoid \n if we don't have construction if
             if line.strip():
-                nginx_log_list.append(line)
-    return nginx_log_list
+                code_500 += log_parser_code_500(line)
+                nginx_parser(line)
+                count_method_code = count_request(line,count_method_code)
+        db_session.commit()
+        print("Records number with CODE =200 and METHOD = GET: {}".format(count_method_code))
+
+    write_new_log(code_500)   
+    print("Log with CODE=500 was write in new file")    
 
 
-def log_parser_code_500(nginx_log):
-    code_500 = []
-    print(nginx_log) 
-    for line in nginx_log:
+def log_parser_code_500(line):
+        code_500 = []
         item = line.split(" ")
         if item[8] == "500":
             code_500.append(line)
-    return code_500
+        return code_500
 
 
 def write_new_log(log_list):
@@ -31,42 +35,38 @@ def write_new_log(log_list):
             log_500.write(line)
 
 
-def count_request(log_list):
-    count_req = 0
-    for line in log_list:
-        item = line.split(" ")
-        if item[8] == "200" and item[5] == "GET":
+def count_request(line, count_req=0):
+    item = line.split(" ")
+    method_post = item[5].strip("\"")
+    if item[8] == "200" and method_post == "GET":
             count_req += 1
     return count_req
 
 
-def nginx_parser(log_list):
-    # TODO try to use this iteration once in file
-    for line in log_list:
-        nginx_data = {}
-        item = line.split(" ")
-        nginx_data["ip"] = item[0]
-        nginx_data["method"] = item[5].strip("\"")
-        nginx_data["code"] = item[8] 
-        import_to_db(nginx_data)  
+def nginx_parser(line):
+    nginx_data = {}
+    item = line.split(" ")
+    nginx_data["ip"] = item[0]
+    nginx_data["method"] = item[5].strip("\"")
+    nginx_data["code"] = item[8] 
+    import_to_db(nginx_data)  
 
 
 def import_to_db(dict_row):
     log = Log(dict_row.get('ip'), dict_row.get('method'), dict_row.get('code'))
     db_session.add(log)
-    db_session.commit()   
+       
 
 
 def delete_records():
     try:
         db_session.query(Log).delete()
-        db_session.commit()    
+        db_session.commit()
+        return True    
     except:
         db_session.rollback()
-        # TODO add error
-        # Add Error description
         print("Records was not deleted. Commit was rolled ")
-        
+        return False
 
 def count_column(column_name):
     db_count = db_session.query(func.count(distinct(column_name))).scalar()
@@ -83,26 +83,19 @@ def count_ip():
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print("Please type one of the command! count or fill")
+        print("Please type one of the command! count or upload")
         exit()
 
-    if sys.argv[1] == "fill":
+    if sys.argv[1] == "upload":
         file_path = "nginx.log"
-        # TODO if file not exists
         if not os.path.exists(file_path):
             print("File not found")
         else:
-            if file_path.endswith(".txt"):
-                nginx_log = load_log_file(file_path)
-                log_code_500 = log_parser_code_500(nginx_log)
-                print("Request with code 500:\n{}".format(log_code_500))
-                write_new_log(log_code_500)
-        
-                print("-" * 100)
-                # TODO if file have wrong format
-                delete_records()
-                print("All Records in Table deleted!")
-                log_data = nginx_parser(nginx_log) 
+            if file_path.endswith(".log"):
+                if delete_records() is True:
+                    print("All Records in Table deleted!")
+                
+                main_parser_nginx(file_path)             
                 print("Log was upload")            
     elif sys.argv[1] == "count":
         num_ip, num_code, num_method = count_ip()
